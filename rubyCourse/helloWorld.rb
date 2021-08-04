98f0ddb78658c5f4c3317a35956317099498a5e2
Isaac Vance <isaacchristophervance@gmail.com>
2021-08-04 11:10:08 -0600

    string basics

--- rubyCourse/helloWorld.rb
+++ rubyCourse/helloWorld.rb
@@ -1,1 +1,9 @@
-puts "Hello World!"
+system "clear"
+
+#------String Basics------
+
+puts "Hello World!\nLemmiwinks\n\n".downcase
+puts "Hello World!\nLemmiwinks\n\n".upcase
+puts "Hello World!\nLemmiwinks\n\n".capitalize
+puts "Hello World!\nLemmiwinks".reverse
+
