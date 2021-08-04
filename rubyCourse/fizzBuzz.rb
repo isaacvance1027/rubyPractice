b78bda96080bb287a3e071294e977c9a4dd3aa38
Isaac Vance <isaacchristophervance@gmail.com>
2021-08-04 12:08:59 -0600

    while loop fizz buzz

--- rubyCourse/fizzBuzz.rb
+++ rubyCourse/fizzBuzz.rb
@@ -2,12 +2,27 @@ system "clear"
 
 (1..100).each do |i|
     if (i%3 == 0) && (i%5 == 0)
-        puts "Fizz Buzz"
+        puts "#{i} Fizz Buzz"
     elsif i%5 == 0
-        puts "Fizz"
+        puts "#{i} Buzz"
     elsif i%3 == 0
-        puts "Buzz"
+        puts "#{i} Fizz"
     else
         puts i
     end
+end
+
+i = 1
+
+while i < 101
+    if (i%3 == 0) && (i%5 == 0)
+        puts "#{i} Fizz Buzz"
+    elsif i%5 == 0
+        puts "#{i} Buzz"
+    elsif i%3 == 0
+        puts "#{i} Fizz"
+    else
+        puts i
+    end
+    i +=1
 end
