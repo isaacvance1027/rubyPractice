865d57f494df641eed6e54f92a32ca4c4bd5b25c
Isaac Vance <isaacchristophervance@gmail.com>
2021-08-04 11:56:16 -0600

    each loop basics

--- rubyCourse/loops.rb
+++ rubyCourse/loops.rb
@@ -7,3 +7,14 @@ while num < 10
     puts num
     num += 1
 end
+
+#---each loops---
+(1..5).each do |i|
+    puts i
+end
+
+names = ["John", "Isaac", "Lemmiwinks"]
+
+names.each do |name|
+    puts name
+end
